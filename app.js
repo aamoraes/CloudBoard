9d1ec5059dd3a83d903e04f84447ae385a8276ef
Chirag Patel <chiragpat@gmail.com>
2013-09-24 17:54:53 -0500

    init server

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var express = require('express'),
+    http    = require('http'),
+    path    = require('path'),
+    dust = require('consolidate').dust,
+    MongoStore = require('connect-mongo')(express);
+
+
+var app = express();
+
+app.engine('dust', dust);
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'dust');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: 'shhitsasecret',
+  store: new MongoStore({
+    'url': process.env.CLOUDBOARD_DB_URL,
+    'clear_interval': 3600
+  })
+}));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
